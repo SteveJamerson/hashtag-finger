1ec0b69e64b157851085463dc7d79e393db48b8a
SteveJamerson <stevejamerson@outlook.com>
2022-03-11 20:34:46 -0300

    feat: adding align, wrap, size, variant, margin; created interface; style

--- src/component/atoms/Text/style.ts
+++ src/component/atoms/Text/style.ts
@@ -1,6 +1,27 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from 'styled-components';
+import { TextProps } from './interface';
 
-export const TextComponent = styled.p`
-    color: ${props => props.color === 'primary' ? 'black' : 'white'};
-    font-weight: 400;
-`
+const weight = {
+   light: 300,
+   regular: 400,
+   medium: 500,
+   'semi-bold': 600,
+   bold: 700,
+   'extra-bold': 800,
+   bolder: 900,
+};
+
+export const TextComponent = styled.p<TextProps>`
+   color: ${(props) => (props.color === 'primary' ? '#FFFFFF' : '#D9D9D9')};
+   font-weight: ${(props) => props.weight && weight[props.weight]};
+   font-size: ${(props) => props.size};
+   text-align: ${(props) => props.align};
+   white-space: ${(props) => props.wrap};
+   margin: ${(props) => props.margin};
+
+   ${(props) =>
+      props.variant === 'heading' &&
+      css`
+         font-weight: ${weight.bold};
+      `};
+`;
