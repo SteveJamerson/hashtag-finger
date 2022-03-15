16622bac7e1f8f66f78198aedeb5cab5280b144e
DanielleIzawa <izawa.danielle@gmail.com>
2022-03-14 21:44:29 -0300

    feat:create route about page

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
+import About from './About';
 
 export default function Routing() {
    return (
       <Routes>
          <Route path="/home" element={<Home />} />
+         <Route path="/about" element={<About />} />
       </Routes>
    );
 }
